8c08cd6b9a672b700fc4d03127a0a8d8a8281feb
didwldk <118715150+didwldk@users.noreply.github.com>
2023-10-21 03:21:04 +0900

    login 화면 제작 1차

---
+++ js/login_join.js
@@ -0,0 +1,9 @@
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
+const navListUl = document.getElementsByClassName("nav-login")[0];
+nav_toggle.onclick = () => {
+    // 햄버거 메뉴 클릭 -> 햄버거 메뉴 i "bi bi-list" <-> X "bi bi-x-lg"
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+    // 햄버거 메뉴 클릭 -> .nav-list에 .show-menu 보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
