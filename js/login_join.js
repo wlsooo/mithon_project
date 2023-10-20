d30132b56458e2a20713388dcde790e83be0fe7e
didwldk <118715150+didwldk@users.noreply.github.com>
2023-10-21 03:27:24 +0900

    login 유저 아이콘 변경 기존 : 햄버거 메뉴 -> 유저 아이콘

--- js/login_join.js
+++ js/login_join.js
@@ -3,7 +3,7 @@ const navListUl = document.getElementsByClassName("nav-login")[0];
 nav_toggle.onclick = () => {
     // 햄버거 메뉴 클릭 -> 햄버거 메뉴 i "bi bi-list" <-> X "bi bi-x-lg"
     navToggleI.classList.toggle("bi-list");
-    navToggleI.classList.toggle("bi-x-lg");
+    navToggleI.classList.toggle("bi-bi-person-circle");
     // 햄버거 메뉴 클릭 -> .nav-list에 .show-menu 보여주기 <-> 안보여주기
     navListUl.classList.toggle("show-menu");
 }
